9985483ebd0d7fe43013736726a6e60c2984aab4
Federico Poggi <federicopoggi3@gmail.com>
2024-02-09 09:20:58 +0100

    Init BackEnd Structure

---
+++ BackEndHealthyNutritionLAb/src/main/java/org/federicopoggi/backendhealthynutritionlab/entities/Diet.java
@@ -0,0 +1,37 @@
+package org.federicopoggi.backendhealthynutritionlab.entities;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "diets")
+@NoArgsConstructor
+public class Diet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diet_id")
+    long dietId;
+
+}
+/*
+@Entity
+public class Dieta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "dieta_alimento_quantita",
+            joinColumns = @JoinColumn(name = "dieta_id")
+    )
+    @MapKeyJoinColumn(name = "alimento_id")
+    @Column(name = "quantita")
+    private Map<Alimento, Integer> alimentiQuantita = new HashMap<>();
+
+    // Metodi getter e setter per alimentiQuantita
+
+    // Altri attributi e metodi della classe Dieta
+}*/
